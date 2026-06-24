8fe1207ba95866c26976cbc16f7e5e6c954628b0
10xdev4u-alt <10xdev4u@gmail.com>
2026-06-25 00:10:10 +0530

    feat: add request ID, timeout, and recovery middleware

---
+++ apps/api/internal/handlers/recovery_middleware.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+	"runtime/debug"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RecoveryMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				stack := debug.Stack()
+				_ = stack
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": "internal server error",
+				})
+				c.Abort()
+			}
+		}()
+		c.Next()
+	}
+}
